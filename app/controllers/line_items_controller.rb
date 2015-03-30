7c2613403107ecafb0014fec152c320213996c2c
buzzlightyear182 <jane.buzzlightyear@gmail.com>
2015-03-30 23:26:22 +0800

    Update shopping cart sidebar via AJAX

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -33,6 +33,7 @@ class LineItemsController < ApplicationController
       if @line_item.save
         format.html { redirect_to root_path, notice: 'Line item was successfully created.' }
         format.json { render :show, status: :created, location: @line_item }
+        format.js
       else
         format.html { render :new }
         format.json { render json: @line_item.errors, status: :unprocessable_entity }
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def hidden_div_if(condition, attributes = {}, &block)
+    if condition
+      attributes["style"] = "display: none"
+    end
+    content_tag("li", attributes, &block)
+  end
 end
