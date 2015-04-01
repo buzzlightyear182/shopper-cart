cced7f47a924bde42e14e6937f6df5ebe49d1212
buzzlightyear182 <jane.buzzlightyear@gmail.com>
2015-04-01 17:20:36 +0800

    Can switch between locales

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -42,7 +42,7 @@ class OrdersController < ApplicationController
         session[:cart_id] = nil
         OrderNotifier.received(@order).deliver
 
-        format.html { redirect_to store_path, notice: 'Thank you for your order.' }
+        format.html { redirect_to store_path, notice: I18n.t('.thanks') }
         format.json { render :show, status: :created, location: @order }
       else
         format.html { render :new }
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -3,6 +3,10 @@ class StoreController < ApplicationController
   before_action :set_cart
 
   def index
-    @products = Product.all
+    if params[:set_locale]
+      redirect_to store_url(locale: params[:set_locale])
+    else
+      @products = Product.all
+    end
   end
 end
