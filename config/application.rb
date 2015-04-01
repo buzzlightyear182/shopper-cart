b26d758af501b516b9aaa75b1ad2ab6538cc53db
buzzlightyear182 <jane.buzzlightyear@gmail.com>
2015-04-01 20:32:38 +0800

    Config fix

--- config/application.rb
+++ config/application.rb
@@ -22,10 +22,7 @@ module Shopper
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-     config.assets.paths << Rails.root.join("vendor","assets","bower_components")
-    config.assets.paths << Rails.root.join("vendor","assets","bower_components","bootstrap-material-design","assets","fonts")
-
-    config.assets.precompile << %r(.*.(?:eot|svg|ttf|woff)$)
-    config.assets.enabled = true
+    config.assets.paths << Rails.root.join("vendor","assets","bower_components")
+    config.serve_static_assets = true
   end
 end
