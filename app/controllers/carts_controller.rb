052e2b77aab8a33fd660282ae79e183de9dbd1bd
buzzlightyear182 <jane.buzzlightyear@gmail.com>
2015-04-01 17:23:06 +0800

    Fixed root to store

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -58,7 +58,7 @@ class CartsController < ApplicationController
     @cart.destroy if @cart.id == session[:cart_id]
     session[:cart_id] = nil
     respond_to do |format|
-      format.html { redirect_to root_path, notice: 'Cart was successfully destroyed.' }
+      format.html { redirect_to store_path, notice: 'Cart was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
@@ -76,6 +76,6 @@ class CartsController < ApplicationController
 
     def invalid_cart
       logger.error "Attempt to access invalid cart #{params[:id]}"
-      redirect_to root_path, notice: 'Invalid cart'
+      redirect_to store_path, notice: 'Invalid cart'
     end
 end
